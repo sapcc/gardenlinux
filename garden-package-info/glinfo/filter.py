f4cebe7bcc01da8b7ca7ccd01bc11a8bf57bd16d
Vincent Riesop <v.riesop@sap.com>
2022-03-25 14:22:19 +0000

    feat(package info): specify attr to include

--- garden-package-info/glinfo/filter.py
+++ garden-package-info/glinfo/filter.py
@@ -11,8 +11,10 @@ import glinfo.common as glinfo
 
 @click.command(name="filter")
 @click.option("--by", type=(str, str), multiple=True, help='Search package name')
+@click.option("--select", type=str, multiple=True, help='Only include given attributes in output')
+@click.option("--only-names", is_flag=True, default=False, help='Only include names in output, no attributes')
 @click.option("--input-yaml", default="packages.yaml", type=click.Path(exists=True), help='Path to packages.yaml generated via fetch command')
-def pfilter(by, input_yaml):
+def pfilter(by, select, only_names, input_yaml):
     with open(input_yaml, 'r') as file:
         packages_dict = yaml.safe_load(file)
 
@@ -22,6 +24,16 @@ def pfilter(by, input_yaml):
             for pkgk in list(packages_dict[archk]):
                 if not glinfo.any_match(fk, fv, packages_dict[archk][pkgk]):
                     del packages_dict[archk][pkgk]
+
+    for archk in list(packages_dict):
+        for pkgk in list(packages_dict[archk]):
+            for attribute in list(packages_dict[archk][pkgk]):
+                if only_names:
+                    del packages_dict[archk][pkgk][attribute]
+                    continue
+                if not attribute in select:
+                    del packages_dict[archk][pkgk][attribute]
+
 
     yaml.dump(packages_dict, sys.stdout)
 
