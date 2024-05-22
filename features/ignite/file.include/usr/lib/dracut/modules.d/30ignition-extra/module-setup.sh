14f023cd519357832939799a1c64bf1c71f3b59d
Fabian Wiesel <fabian.wiesel@sap.com>
2024-05-22 11:39:29 +0200

    [orabos] Move patched ignition to its own feature
    It breaks the cloud-init based setup, so we need to move
    it to the pipeline and generate two images (for now).

--- features/ignite/file.include/usr/lib/dracut/modules.d/30ignition-extra/module-setup.sh
+++ features/ignite/file.include/usr/lib/dracut/modules.d/30ignition-extra/module-setup.sh
@@ -8,4 +8,5 @@ install() {
     # ignition environment
     inst_script "$moddir/ignition-env-generator.sh" $systemdutildir/system-generators/ignition-env-generator
     inst_simple "$moddir/ignition-files.env" /etc/ignition-files.env
+    inst_simple "$moddir/ignition" /usr/bin/ignition
 }
