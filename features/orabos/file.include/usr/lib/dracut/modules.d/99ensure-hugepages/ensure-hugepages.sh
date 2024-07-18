142e2fa4aac8160733dee2bec256356dc2827e6d
Fabian Wiesel <fabian.wiesel@sap.com>
2024-07-18 13:25:05 +0200

    [orabos] Tune vm.watermark_scale_factor
    The value is scaling with the amount of RAM in the system,
    but we take a way a large chunk for huge-pages.

--- features/orabos/file.include/usr/lib/dracut/modules.d/99ensure-hugepages/ensure-hugepages.sh
+++ features/orabos/file.include/usr/lib/dracut/modules.d/99ensure-hugepages/ensure-hugepages.sh
@@ -4,21 +4,37 @@
 
 hugepages=$(getarg hugepages=) || hugepages=0
 
-if [ $hugepages -gt 0 ]; then
+mem_total_mb=$(($(sed -rn 's/MemTotal:\s+(.*) kB/\1/p' /proc/meminfo) / 1024 ))
+hugepagesize_mb=$(($(sed -rn 's/Hugepagesize:\s+(.*) kB/\1/p' /proc/meminfo) / 1024 ))
+
+function adopt_watermark_scale_factor() {
+  # On a 3TiB host, the default watermark_scale_factor=10 was exactly that
+  # that the kswapd0 was running permanently. Setting it to 5 was solving the
+  # issue, but is likely a suboptimal value, but a first start.
+  # The value 500 reproduces exactly that value for that scale, and hopefully
+  # also holds for larger hosts.
+  max_watermark_scale_factor=$(($non_hugepages_mb * 500 / $mem_total_mb))
+  watermark_scale_factor=$(</proc/sys/vm/watermark_scale_factor)
+  if [ $max_watermark_scale_factor -lt $watermark_scale_factor ]; then
+    echo $max_watermark_scale_factor > /proc/sys/vm/watermark_scale_factor
+  fi
+}
+
+if [ ${hugepages:-0} -gt 0 ]; then
+  hugepages_mb=$(($hugepages * $hugepagesize_mb))
+  non_hugepages_mb=$(($mem_total_mb - $hugepages_mb))
+  adopt_watermark_scale_factor
   exit 0
 fi
 
 non_hugepages_mb=$(getarg rd.non_hugepages_mb=) || non_hugepages_mb=32768
+hugepages=$((($mem_total_mb - $non_hugepages_mb) / $hugepagesize_mb))
 
-mem_total_mb=$(($(sed -rn 's/MemTotal:\s+(.*) kB/\1/p' /proc/meminfo) / 1024 ))
-hugepagesize_mb=$(($(sed -rn 's/Hugepagesize:\s+(.*) kB/\1/p' /proc/meminfo) / 1024 ))
-nr_hugepages=$((($mem_total_mb - $non_hugepages_mb) / $hugepagesize_mb))
-
-if [ $nr_hugepages -le 0 ]; then
+if [ $hugepages -le 0 ]; then
   exit 0
 fi
 
-cmdline="$(</proc/cmdline) hugepages=$nr_hugepages"
+cmdline="$(</proc/cmdline) hugepages=$hugepages"
 release=$(uname -r)
 
 NEWROOT=${NEWROOT:-/sysroot}
